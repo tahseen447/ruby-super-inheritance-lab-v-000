3b6738e32d4898009c8fb9acd59501609ff27b19
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-12 22:22:20 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,9 +2,8 @@ class ChattyStudent < Student
 
   def hello
     super
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Wal
-king Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoile
-rs? Okay well let me just tell you who died..."
+    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
+
 
 end
