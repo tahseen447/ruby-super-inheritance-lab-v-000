70b9f17817d667a634c79ab04e31f6fcea0c50e3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-12 22:21:59 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,3 +1,10 @@
 class ChattyStudent < Student
 
+  def hello
+    super
+    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Wal
+king Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoile
+rs? Okay well let me just tell you who died..."
+end
+
 end
